34dacc4a4422d3936a22951ce3529b87b77e791d
alexmirmao <66200733+alexmirmao@users.noreply.github.com>
2020-05-31 11:20:53 +0200

    Add files via upload
    Funcion que recorre todos los directorios y subdirectorios dentro del path actual en busqueda de archivos en esas carpetas y subcarpetas.

---
+++ funcion_recorrer_archivos.py
@@ -0,0 +1,20 @@
+"""
+@author: alexmirmao
+@mail: alexmirmao@gmail.com
+"""
+import os
+def recorre_archivos(directorio_actual):
+    #imprime tanto los archivos como los directorios en el primer nivel del directorio actual
+    lista_directorios = os.listdir(directorio_actual)
+    #lista directorios guarda todos los archivos y 
+    #directorios de primer nivel del directorio en el 
+    # que nos encontramos
+    for directorio_files in lista_directorios:
+        directorio_files = directorio_actual + "\\" + directorio_files
+        if os.path.isfile(directorio_files):
+            print("archivo: " + directorio_files)
+            
+        else:
+            
+            recorre_archivos(directorio_files)
+
